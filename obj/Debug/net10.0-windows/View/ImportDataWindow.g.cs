4c0433a0d68a6e63071cd737f11eeab0ed4d05c9
blessstudiosie <blessstudiosie@gmail.com>
2026-08-07 07:10:26 +0800

    ambil

--- obj/Debug/net10.0-windows/View/ImportDataWindow.g.cs
+++ obj/Debug/net10.0-windows/View/ImportDataWindow.g.cs
@@ -123,7 +123,7 @@ namespace SPJ_APP.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SPJ APP;component/view/importdatawindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SPJ APP;V1.0.0.0;component/view/importdatawindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\ImportDataWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
